ff0e92c72b3a72e24e03fa23f46e2c902b367bc6
Simon Duflot <narcisse.duflot@gmail.com>
2023-11-30 12:34:29 +0100

    Node.js Backend, test

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var router = express.Router();
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
+
+/* GET home page. */
+const uniqid = require("uniqid");
+
+router.post("/upload", async (req, res) => {
+  const photoPath = `./tmp/${uniqid()}.jpg`;
+  const resultMove = await req.files.photoFromFront.mv(photoPath);
+  console.log(req.files.photoFromFront);
+  if (!resultMove) {
+    const resultCloudinary = await cloudinary.uploader.upload(photoPath);
+    res.json({ result: true, url: resultCloudinary.secure_url });
+  } else {
+    res.json({ result: false, error: resultMove });
+  }
+  fs.unlinkSync(photoPath);
+});
+
+module.exports = router;
